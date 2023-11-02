7d2f32b9c4db9953c5a8a1a5eeacd00395f5a960
jfx2m <jfambar2109@iesalbox.com>
2023-11-02 13:27:21 +0100

    First Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
+      port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
